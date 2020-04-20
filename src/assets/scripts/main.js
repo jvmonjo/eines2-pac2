38df8248615ddbefdd8df01bd4fbb51c2a1d8929
monjo <jvmonjo@gmail.com>
2020-04-20 10:54:28 +0200

    ADDED: bootstrap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,9 +2,7 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import 'bootstrap';
 
 /**
  * Write any other JavaScript below
