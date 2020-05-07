5c150310b2d6db7f847835856dbbd356bf788019
monjo <jvmonjo@gmail.com>
2020-05-07 15:52:20 +0200

    FIXED: font awesome loaded in the html

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,9 +2,8 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/css/all.css';
+
 
 /**
  * Write any other JavaScript below
