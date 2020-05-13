0ada1d9d4b92fb81b99d73d6cd5a8600c68899b3
monjo <jvmonjo@gmail.com>
2020-05-13 11:33:33 +0200

    CHANGED bootstrap import sequence

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,14 +2,13 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-import '@fortawesome/fontawesome-free/js/all.min.js'
+import '@fortawesome/fontawesome-free/js/all.min.js';
 import 'bootstrap';
-
 
 /**
  * Write any other JavaScript below
  */
 
 +( function() {
-  console.log('Hello, UOC!');
+  // console.log('Hello, UOC!');
 } )();
